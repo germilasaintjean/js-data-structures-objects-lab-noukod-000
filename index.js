176404b0e80ebd9592f4b0ed1245bc69e6122138
Saint-Jean Germila <germila@noukod.com>
2019-03-06 16:15:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const driver = {};
 
 function updateDriverWithKeyAndValue (driver, key, value) {
   return Object.assign({}, driver, {[key]:value});
+}
 
+function destructivelyUpdateDriverWithKeyAndValue (driver, key, value) {
+  const obj = {street: "11 Broadway"};
+  return Object.assign({}, obj);
 }
